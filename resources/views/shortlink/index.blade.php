59a6039a3296936ee525ef2ebb070660edf0eee5
Sergo Khachatryan <sergokhachatryan97@gmail.com>
2026-03-05 17:59:26 +0400

    fix

--- resources/views/shortlink/index.blade.php
+++ resources/views/shortlink/index.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Shortlink Generator</title>
+    <meta name="heleket" content="89c70a02" />
+    <title>Trastly</title>
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
